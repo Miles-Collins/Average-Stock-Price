cce0f7072c992d960db60c4c43637f8612279994
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2024-10-03 00:41:07 -0700

    changed if's to switch

--- average_stock_price/src/main/java/com/main/DashedBoxOptions.java
+++ average_stock_price/src/main/java/com/main/DashedBoxOptions.java
@@ -21,55 +21,61 @@ public class DashedBoxOptions {
 
         boolean exit = false;
 
+        OUTER:
         while (true) {
-            // Display the options in a dashed box
             dashedBox.displayOptionsInBox(options);
-
-            // Get user's choice
             System.out.print("Please enter the number of your choice: ");
             int choice = scanner.nextInt();
-
             if (choice == options.length) {
                 System.out.println("Exiting the program. Goodbye!");
-                break;  // Exit after showing the box again
+                break;
             } else {
                 System.out.println("You selected: " + options[choice - 1]);
             }
-
-            if (choice == 1) {
-                Scanner firstScan = new Scanner(System.in);
-                System.err.println("How many shares would you like to buy?");
-                double shares = scanner.nextDouble();
-                Scanner secondScan = new Scanner(System.in);
-                System.err.println("How much is each share?");
-                double price = scanner.nextDouble();
-                company.buy(shares, price);
-                String totalCapital = company.getCapital();
-                System.out.println("Your capital is now: " + totalCapital);
-            } else if (choice == 4) {
-                String totalCapital = company.getCapital();
-                System.out.println("Your total capital is: " + totalCapital);
-            } else if (choice == 3) {
-                String shares = company.getShares();
-                System.out.println("You have a total of " + shares + " in your portfolio!");
-            } else if (choice == 2) {
-                String s = company.getShares();
-                if (Double.parseDouble(s) >= 0) {
-                    System.err.println("Sorry, you do not have any shares to sell!");
+            switch (choice) {
+                case 1: {
+                    Scanner firstScan = new Scanner(System.in);
+                    System.err.println("How many shares would you like to buy?");
+                    double shares = scanner.nextDouble();
+                    Scanner secondScan = new Scanner(System.in);
+                    System.err.println("How much is each share?");
+                    double price = scanner.nextDouble();
+                    company.buy(shares, price);
+                    String totalCapital = company.getCapital();
+                    System.out.println("Your capital is now: " + totalCapital);
                     break;
                 }
-                Scanner firstScan = new Scanner(System.in);
-                System.out.println("How many shares would you like to sell?");
-                double shares = scanner.nextDouble();
-                Scanner secondScan = new Scanner(System.in);
-                System.out.println("How much are you selling each share?");
-                double price = scanner.nextDouble();
-                company.sell(shares, price);
-                String totalShares = company.getShares();
-                System.out.println("Your capital is now: " + totalShares);
+                case 4: {
+                    String totalCapital = company.getCapital();
+                    System.out.println("Your total capital is: " + totalCapital);
+                    break;
+                }
+                case 3: {
+                    String shares = company.getShares();
+                    System.out.println("You have a total of " + shares + " in your portfolio!");
+                    break;
+                }
+                case 2: {
+                    String s = company.getShares();
+                    if (Double.parseDouble(s) >= 0) {
+                        System.err.println("Sorry, you do not have any shares to sell!");
+                        break OUTER;
+                    }
+                    Scanner firstScan = new Scanner(System.in);
+                    System.out.println("How many shares would you like to sell?");
+                    double shares = scanner.nextDouble();
+                    Scanner secondScan = new Scanner(System.in);
+                    System.out.println("How much are you selling each share?");
+                    double price = scanner.nextDouble();
+                    company.sell(shares, price);
+                    String totalShares = company.getShares();
+                    System.out.println("Your capital is now: " + totalShares);
+                    break;
+                }
+                default:
+                    break;
             }
-
-            System.out.println(""); // Print a blank line before prompting again
+            System.out.println("");
         }
     }
 }
